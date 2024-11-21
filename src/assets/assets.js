3a216bf9b7e0102dc65f4258b5fd603427a3a3f4
Niranjanreddyv <niranjanreddyvakareddy999@gmail.com>
2024-11-21 13:12:01 +0530

    first commit

---
+++ src/assets/assets.js
@@ -0,0 +1,236 @@
+import basket_icon from './basket_icon.png'
+import logo from './logo.png'
+import header_img from './wal.jpeg'
+import search_icon from './search_icon.png'
+import menu_1 from './imgp4.jpg'
+import menu_2 from './imgp6.jpg'
+import menu_3 from './imgp5.jpg'
+import menu_4 from './imgp17.jpg'
+
+
+import food_1 from './alaskan-white.jpg'
+import food_2 from './imgp2.jpg'
+import food_3 from './imgp3.jpg'
+import food_4 from './imgp4.jpg'
+import food_5 from './imgp5.jpg'
+import food_6 from './imgp6.jpg'
+import food_7 from './imgp7.jpg'
+import food_8 from './imgp8.jpg'
+import food_9 from './imgp9.jpg'
+import food_10 from './img10.jpg'
+import food_11 from './imgp11.jpg'
+import food_12 from './imgp12.jpg'
+import food_13 from './imgp13.jpg'
+import food_14 from './imgp14.jpg'
+import food_15 from './imgp15.jpg'
+import food_16 from './imgp16.jpg'
+import food_17 from './imgp17.jpg'
+import food_18 from './imgp18.jpg'
+import food_19 from './imgp19.jpg'
+import food_20 from './imgp20.jpg'
+
+
+
+import add_icon_white from './add_icon_white.png'
+import add_icon_green from './add_icon_green.png'
+import remove_icon_red from './remove_icon_red.png'
+import app_store from './app_store.png'
+import play_store from './play_store.png'
+import linkedin_icon from './linkedin_icon.png'
+import facebook_icon from './facebook_icon.png'
+import twitter_icon from './twitter_icon.png'
+import cross_icon from './cross_icon.png'
+import selector_icon from './selector_icon.png'
+import rating_starts from './rating_starts.png'
+import profile_icon from './profile_icon.png'
+import bag_icon from './bag_icon.png'
+import logout_icon from './logout_icon.png'
+import parcel_icon from './parcel_icon.png'
+
+export const assets = {
+    logo,
+    basket_icon,
+    header_img,
+    search_icon,
+    rating_starts,
+    add_icon_green,
+    add_icon_white,
+    remove_icon_red,
+    app_store,
+    play_store,
+    linkedin_icon,
+    facebook_icon,
+    twitter_icon,
+    cross_icon,
+    selector_icon,
+    profile_icon,
+    logout_icon,
+    bag_icon,
+    parcel_icon
+}
+
+export const menu_list = [
+    {
+        menu_name: "Blcak",
+        menu_image: menu_1
+    },
+    {
+        menu_name: "White",
+        menu_image: menu_2
+    },
+    {
+        menu_name: "Red",
+        menu_image: menu_3
+    },
+    {
+        menu_name: "gray",
+        menu_image: menu_4
+    }
+]
+
+export const food_list = [
+    {
+        _id: "1",
+        name: "alaskan-white",
+        image: food_1,
+        price: 122,
+        description: "Alaskan White Granite is not from Alaska. It is a porphyritic garnetiferous leucogranite from the Ordovician of Brazil. This rock has plagioclase feldspar, quartz, mica, potassium feldspar, and garnet",
+        category: "Granite"
+    },
+    {
+        _id: "2",
+        name: "Black Cambrian",
+        image: food_2,
+        price: 188,
+        description: "A Precambrian norite from Quebec, Canada. Geologic unit & age: Lac-St.-Jean Anorthosite Suite, late Mesoproterozoic, 1.14 to 1.16 b.y. Locality: quarry at St. Nazaire, Chicoutimi County, southern Quebec, southeastern Canad",
+        category: "Granite"
+    }, {
+        _id: "3",
+        name: "Black Galaxy",
+        image: food_3,
+        price: 160,
+        description: "A Proterozoic-aged norite from India’s Eastern Ghats Orogenic Belt. This is probably the most popular and attractive black granite available in the commercial decorative stone trade",
+        category: "Granite"
+    }, {
+        _id: "4",
+        name: "Black Pearl",
+        image: food_4,
+        price: 240,
+        description: "A Proterozoic-aged gabbro from Guntur District, eastern Andhra Pradesh State, southeastern India.",
+        category: "Granite"
+    }, {
+        _id: "5",
+        name: "Cripple Creek",
+        image: food_5,
+        price: 140,
+        description: "This is the Cripple Creek Granite, a Mesoproterozoic-aged intrusive igneous body that dates to 1.46 billion years old. Meyer et al. (2004) describes it as a finely crystalline to medium crystalline granite rich in potassium feldspar, with muscovite & biotite mica.",
+        category: "granite"
+    }, {
+        _id: "6",
+        name: "Delicatus Ice",
+        image: food_6,
+        price: 125,
+        description: "Graphic granite in pegmatitic granite, attributed to Brazil with creamy-colored & white potassium feldspar, light gray to medium gray quartz, and black biotite mica. The small, scattered red spots are garnet (click on the photo to zoom in & look around).",
+        category: "granite"
+    }, {
+        _id: "7",
+        name: "Dikes in Granite",
+        image: food_7,
+        price: 200,
+        description: "Igneous dikes are planar to irregularly-planar igneous intrusions that cut across country rocks. They can be thin or extremely thick & they can be vertical or obliquely-oriented. The cut-and-polished rock shown here has 3 different generations of igneous dikes cutting through.",
+        category: "granite"
+    }, {
+        _id: "8",
+        name: "Dakota Mahogany",
+        image: food_8,
+        price: 150,
+        description: "Dakota Mahogany Granite is a porphyritic granite from the Milbank Granite (late Neoarchean, 2.6-2.7 billion years). It is quarried just east of Milbank, in northeastern Grant County, northeastern South Dakota, USA",
+        category: "granite"
+    }, {
+        _id: "9",
+        name: "Emerald Pearl",
+        image: food_9,
+        price: 145,
+        description: "Larvikite is dominated by large crystals of spectacularly bluish-iridescent (schillerescent) perthitic feldspar (closely intergrown potassium feldspar and plagioclase feldspar). ",
+        category: "granite"
+    }, {
+        _id: "10",
+        name: "Epidotic Dike",
+        image: food_10,
+        price: 222,
+        description: "This is part of a famous outcrop in northeastern Minnesota - Confusion Hill. These are intrusive igneous rocks (principally dark-colored diorite at this spot) of Late Archean age, part of the Giants Range Batholith.",
+        category: "granite"
+    }, {
+        _id: "11",
+        name: "Epidotic Dike 3",
+        image: food_11,
+        price: 100,
+        description: "Roadcut on the eastern side of northbound Rt. 169/Rt. 53, immediately north of roadside rest stop, northwestern side of Lookout Mountain, Laurentian Divide, just north of the town of Virginia, central St. Louis County, northeastern Minnesota, USA",
+       category: "granite"
+    }, {
+        _id: "12",
+        name: "Giallo Napolean",
+        image: food_12,
+        price: 127,
+        description: "Giallo Napoleone Granite is porphyritic alkaline granite from the late Neoproterozoic to Cambrian of Espirito Santo State in southeastern Brazil. Alkaline granites have a higher potassium and/or sodium content than ordinary granites",
+        category: "granite"
+    },
+    {
+        _id: "13",
+        name: "Golden Lightning",
+        image: food_13,
+        price: 140,
+        description: "This material from Iran has been identified as mylonitic epidote gneiss, but I dont see characteristics consistent with that assessment.",
+        category: "granite"
+    },
+    {
+        _id: "14",
+        name: "Graphic",
+        image: food_14,
+        price: 184,
+        description: "Graphic granite (a.k.a. runic granite; a.k.a. runite) has an odd but distinctive type of crystalline texture. Graphic granites are typically bimineralic (K-feldspar & quartz) and consist of interpenetrating crystals",
+        category: "granite"
+    }, {
+        _id: "15",
+        name: "Graphic 2",
+        image: food_15,
+        price: 160,
+        description: "Graphic granite with whitish-gray K-feldspar and dark gray quartz. Geologic unit & age: Ruggles Pegmatite, Grafton Pegmatite Field, New Hampshire Plutonic Series, Devonian",
+        category: "granite"
+    }, {
+        _id: "16",
+        name: "Graphic 3",
+        image: food_16,
+        price: 248,
+        description: "Graphic granite (~8 cm across) from the Precambrian of Russia's Kola Peninsula. Geologic unit & age - apparently from a Paleoproterozoic granite pegmatite intrusion, Belomorian Terrane (Kola Province), Baltic Shield (Fennoscandian Shield).",
+        category: "granite"
+    }, {
+        _id: "17",
+        name: "Iguana Green",
+        image: food_17,
+        price: 147,
+        description: "Iguana Green Granite - faulted and sheared gneiss (or migmatite) from Brazil. Gneiss is a common high-grade, foliated metamorphic rock characterized by alternating bands of light- and dark-colored minerals.",
+        category: "granite"
+    }, {
+        _id: "18",
+        name: "Impala Black",
+        image: food_18,
+        price: 124,
+        description: "An attractive, 2 billion year old gabbronorite from South Africa. It is composed principally of grayish plagioclase feldspar and black pyroxene. It comes from the famous Bushveld Complex, a world-class example of an LLI (large layered igneous province)",
+        category: "granite"
+    }, {
+        _id: "19",
+        name: "Indian Absolute Black",
+        image: food_19,
+        price: 200,
+        description: "The name Absolute Black Granite has been applied to relatively finely-crystalline mafic intrusive igneous rocks from many localities on different continents (e.g., Africa, India, China). This sample is from a large Precambrian",
+        category: "granite"
+    }, {
+        _id: "20",
+        name: "Juperana Colombo",
+        image: food_20,
+        price: 159,
+        description: "Precambrian migmatite from India. Migmatites are very high grade metamorphic rocks that form by partial melting of gneisses. The felsic minerals have melted, cooled, and recrystallized - they form the light-colored, crystalline-textured bands in the rock.",
+        category: "granite"
+    }
+]
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import "./Header.css"
+
+const Header = () => {
+  return (
+    <div className='header'>
+      <div className="header-contents">
+        <h2>Order your favourite Granite here</h2>
+        <p>Looking to transform your space? Discover the finest granite selections at GraniteHub! Durable, stylish, and crafted to perfection – elevate your home or business with our premium stone collection, delivered with expert service</p>
+        <button>view menu</button>
+      </div>
+    </div>
+  )
+}
+
+export default Header
